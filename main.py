95744bc3cd7c8301ceb68e4f02b8130c27c70056
qu-genesis <60377132+qu-genesis@users.noreply.github.com>
2022-09-14 18:52:14 +0000

    added the click cli

--- main.py
+++ main.py
@@ -1,51 +1,79 @@
+"""Building a click command line interface that 
+takes in a city and country and returns the linear regression 
+of the average temperature of the city on the year"""
+
+
 # import the dask libraries
-import dask
+from dask.array import from_array
 import dask.dataframe as dd
 from dask.distributed import LocalCluster, Client
 from dask_ml.linear_model import LinearRegression
+import numpy as np
 import click
-
-
 
 # Create a function that loads in the MajorCity Data and Cleans it
 def load_major_city_data():
     """Load the Major City Data and Clean it"""
     # Load the data into a dask dataframe
-    df = dd.read_csv('raw_data/GlobalLandTemperaturesByMajorCity.csv')
+    ddf = dd.read_csv("raw_data/GlobalLandTemperaturesByMajorCity.csv")
     # Turn the dt column into a datetime object
-    df['dt'] = dd.to_datetime(df['dt'])
+    ddf["dt"] = dd.to_datetime(ddf["dt"])
     # Filter out the data that is not in the 20th and 21st century
-    df = df[(df['dt'] > '1900-01-01') & (df['dt'] < '2100-01-01')]
+    ddf = ddf[(ddf["dt"] > "1900-01-01") & (ddf["dt"] < "2100-01-01")]
     # Rename the dt column to date
-    df = df.rename(columns={'dt': 'date'})
+    ddf = ddf.rename(columns={"dt": "date"})
     # Create a new column that is the year
-    df['year'] = df['date'].dt.year
-    return df
+    ddf["year"] = ddf["date"].dt.year
+    return ddf
+
 
 # Create a function that takes a City and Country as input and queries the Major City Data
-def query_major_city_data(city, country, df):
+def query_major_city_data(city, country, ddf):
     """Query the Major City Data and return mean temperature of the city in each year"""
     # Filter the data to the city and country
-    df_city = df[(df['City'] == city) & (df['Country'] == country)]
+    df_city = ddf[(ddf["City"] == city) & (ddf["Country"] == country)]
     # Group the data by year and calculate the mean temperature
-    df_city = df_city.groupby('year').mean()
+    df_city = df_city.groupby("year").mean().reset_index()
     # Return the dataframe
     return df_city
 
+
 # Create a function that runs a linear regression of the average temperature of the city on the year
-def linear_regression(df):
+def linear_regression(ddf):
     """Run a linear regression on the data"""
     # Create a linear regression object
     lr = LinearRegression()
     # Fit the linear regression model
-    lr.fit(df['year'].to_frame(), df['AverageTemperature'])
+    lr.fit(ddf[["year"]].to_dask_array(), ddf[["AverageTemperature"]].to_dask_array())
     # Return the linear regression object
     return lr
 
 
+# Create a click command that takes in city, country and year as input and returns the predicted average temperature
+@click.command()
+@click.option("--city", default="London", help="The city to analyze")
+@click.option("--country", default="United Kingdom", help="The country to analyze")
+@click.option("--year", default=2010, help="The year to predict")
+def main(city, country, year):
+    """Predict the average temperature of a city in a given year"""
+    # create a local cluster in a context manager
+    with LocalCluster() as cluster, Client(cluster) as client:
+        # load the data
+        ddf = load_major_city_data()
+        # query the data
+        df_city = query_major_city_data(city, country, ddf)
+        # run a linear regression
+        lr = linear_regression(df_city)
+        # construct a numpy array of the year
+        year_array = from_array(np.array([year]).reshape(-1, 1))
+        # predict the temperature for the year
+        temp = lr.predict(year_array).compute()
+        # print the temperature
+        print(
+            f"The predicted average temperature in {city}, {country} in {year} is {temp[0]:.1f} degrees Celsius"
+        )
 
-# start the cluster
-cluster = LocalCluster()
-client = Client(cluster)
 
-# Create a function that subset the data 
+# Run the cli
+if __name__ == "__main__":
+    main()
---
+++ test_main.py
@@ -0,0 +1,32 @@
+import dask.dataframe as dd
+import main
+
+# test that the data will load
+def test_load_major_city_data():
+    """Test that the data will load"""
+    ddf = main.load_major_city_data()
+    assert isinstance(ddf, dd.DataFrame)
+    assert ddf.shape[0].compute() > 10000
+    assert ddf.shape[1] == 8
+    assert "year" in ddf.columns
+
+
+# test that the data will query
+def test_query_major_city_data():
+    """Test that the data will query"""
+    ddf = main.load_major_city_data()
+    df_city = main.query_major_city_data("London", "United Kingdom", ddf)
+    assert isinstance(df_city, dd.DataFrame)
+    assert df_city.shape[0].compute() > 100
+    assert df_city.shape[1] == 3
+    assert "year" in df_city.columns
+    assert "AverageTemperature" in df_city.columns
+
+
+# test that the linear regression will run
+def test_linear_regression():
+    """Test that the linear regression will run"""
+    ddf = main.load_major_city_data()
+    df_city = main.query_major_city_data("London", "United Kingdom", ddf)
+    lr = main.linear_regression(df_city)
+    assert hasattr(lr, "predict")
