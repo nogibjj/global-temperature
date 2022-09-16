470c3ee90d93c1125dd1c17f44ae03bad6fef428
qu-genesis <60377132+qu-genesis@users.noreply.github.com>
2022-09-16 02:23:06 +0000

    updated the code to import the larger city dataset

--- main.py
+++ main.py
@@ -6,21 +6,43 @@ of the average temperature of the city on the year"""
 
 
 # import the dask libraries
-from dask.array import from_array
-import dask.dataframe as dd
-from dask_ml.linear_model import LinearRegression
+import zipfile
 import numpy as np
 import click
+import dask.dataframe as dd
+import os.path
+from dask.array import from_array
+from dask_ml.linear_model import LinearRegression
+from kaggle.api.kaggle_api_extended import KaggleApi
 
 # Create a function that loads in the MajorCity Data and Cleans it
-def load_major_city_data():
-    """Load the Major City Data and Clean it"""
-    # Load the data into a dask dataframe
-    ddf = dd.read_csv("raw_data/GlobalLandTemperaturesByMajorCity.csv")
+def load_city_data():
+    """Load the City Data and Clean it"""
+    # check if the city file is in the current directory
+    if os.path.isfile("GlobalLandTemperaturesByCity.csv"):
+        # read the data into a dask dataframe
+        ddf = dd.read_csv("GlobalLandTemperaturesByCity.csv")
+    else:
+        # create a kaggle api object
+        api = KaggleApi()
+        # authenticate with kaggle
+        api.authenticate()
+        # download the data
+        api.dataset_download_file(
+            "berkeleyearth/climate-change-earth-surface-temperature-data",
+            "GlobalLandTemperaturesByCity.csv",
+        )
+        # unzip the data
+        with zipfile.ZipFile("GlobalLandTemperaturesByCity.csv.zip", "r") as zip_ref:
+            zip_ref.extractall()
+        # read the data into a dask dataframe
+        ddf = dd.read_csv("GlobalLandTemperaturesByCity.csv")
+    # drop the rows with missing values
+    ddf = ddf.dropna()
     # Turn the dt column into a datetime object
     ddf["dt"] = dd.to_datetime(ddf["dt"])
     # Filter out the data that is not in the 20th and 21st century
-    ddf = ddf[(ddf["dt"] > "1900-01-01") & (ddf["dt"] < "2100-01-01")]
+    ddf = ddf[ddf["dt"] > "1800-01-01"]
     # Rename the dt column to date
     ddf = ddf.rename(columns={"dt": "date"})
     # Create a new column that is the year
@@ -28,9 +50,9 @@ def load_major_city_data():
     return ddf
 
 
-# Create a function that takes a City and Country as input and queries the Major City Data
-def query_major_city_data(city, country, ddf):
-    """Query the Major City Data and return mean temperature of the city in each year"""
+# Create a function that takes a City and Country as input and queries the  City Data
+def query_city_data(city, country, ddf):
+    """Query the  City Data and return mean temperature of the city in each year"""
     # Filter the data to the city and country
     df_city = ddf[(ddf["City"] == city) & (ddf["Country"] == country)]
     # Group the data by year and calculate the mean temperature
@@ -55,20 +77,27 @@ def linear_regression(ddf):
 @click.option("--city", default="London", help="The city to analyze")
 @click.option("--country", default="United Kingdom", help="The country to analyze")
 @click.option("--year", default=2010, help="The year to predict")
-def main(city, country, year):
+@click.option(
+    "--fahrenheit", default=False, help="Return the temperature in Fahrenheit"
+)
+def main(city, country, year, fareinheit):
+    """Main function that runs the click command"""
     # load the data
-    ddf = load_major_city_data()
+    ddf = load_city_data()
     # query the data
-    df_city = query_major_city_data(city, country, ddf)
+    df_city = query_city_data(city, country, ddf)
     # run a linear regression
     lr = linear_regression(df_city)
     # construct a numpy array of the year
     year_array = from_array(np.array([year]).reshape(-1, 1))
     # predict the temperature for the year
-    temp = lr.predict(year_array).compute()
+    if fareinheit:
+        temp = lr.predict(year_array) * 9 / 5 + 32
+    else:
+        temp = lr.predict(year_array)
     # print the temperature
     print(
-        f"The predicted average temperature in {city}, {country} in {year} is {temp[0]:.1f} degrees Celsius"
+        f"The predicted average temperature in {city}, {country} in {year} is {temp.compute()[0]:.1f} degrees Celsius."
     )
 
 
--- test_main.py
+++ test_main.py
@@ -4,7 +4,7 @@ import main
 # test that the data will load
 def test_load_major_city_data():
     """Test that the data will load"""
-    ddf = main.load_major_city_data()
+    ddf = main.load_city_data()
     assert isinstance(ddf, dd.DataFrame)
     assert ddf.shape[0].compute() > 10000
     assert ddf.shape[1] == 8
@@ -14,8 +14,8 @@ def test_load_major_city_data():
 # test that the data will query
 def test_query_major_city_data():
     """Test that the data will query"""
-    ddf = main.load_major_city_data()
-    df_city = main.query_major_city_data("London", "United Kingdom", ddf)
+    ddf = main.load_city_data()
+    df_city = main.query_city_data("London", "United Kingdom", ddf)
     assert isinstance(df_city, dd.DataFrame)
     assert df_city.shape[0].compute() > 100
     assert df_city.shape[1] == 3
@@ -26,7 +26,7 @@ def test_query_major_city_data():
 # test that the linear regression will run
 def test_linear_regression():
     """Test that the linear regression will run"""
-    ddf = main.load_major_city_data()
-    df_city = main.query_major_city_data("London", "United Kingdom", ddf)
+    ddf = main.load_city_data()
+    df_city = main.query_city_data("London", "United Kingdom", ddf)
     lr = main.linear_regression(df_city)
     assert hasattr(lr, "predict")
