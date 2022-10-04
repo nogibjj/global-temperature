09a76a55b117ecc865892b97e30d361a5ac9cb32
qu-genesis <qu.genesis@gmail.com>
2022-10-03 21:58:07 -0400

    pulled out parquet files

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,17 @@
-FROM python:3.10
-
-WORKDIR /app
-COPY requirements.txt ./requirements.txt
-RUN pip3 install -r requirements.txt
+FROM python:3.9-slim
 
 EXPOSE 8501
-COPY app.py /app/
 
-ENTRYPOINT ["streamlit", "run"]
-CMD ["app.py"]
+WORKDIR /app
+
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    software-properties-common \
+    git \
+    && rm -rf /var/lib/apt/lists/*
+
+ADD app.py raw_data/*.parquet requirements.txt /app/
+
+RUN pip3 install -r requirements.txt
+
+ENTRYPOINT ["streamlit", "run", "app.py", "--server.port=8501", "--server.address=0.0.0.0"]
--- app.py
+++ app.py
@@ -7,20 +7,22 @@ from sklearn.linear_model import LinearRegression
 
 st.title("Global Temperature Predictions")
 
-st.write("""This project allows users to make predictions on the temperature of 
+st.write(
+    """This project allows users to make predictions on the temperature of 
         major cities in the world using historical climate data. The planet has only warmed further since world leaders sat down in 
         Kyoko and signed the Kyoto Protocol in 1997. One driving force of geopolitics in the 21st century will be climate change and 
         the constellation of consequences it will bring. In particular, the coming climate migration will leave a footprint on
         the earth's cultural, demographical and ecological landscape. It is crucial for us to understand where the earth will warm faster
-        and by how much. The project was conceived to further this understanding.""")
-        
+        and by how much. The project was conceived to further this understanding."""
+)
+
 
 # Create a function that loads in the MajorCity Data and Cleans it
 @st.cache
 def load_city_data():
     """Load the City Data and Clean it"""
     # check if the city file is in the current directory
-    parquet_path = glob.glob("raw_data/part*.parquet")
+    parquet_path = glob.glob("*.parquet")
     # import the city file using read_parquet
     if len(parquet_path) > 0:
         ddf = dd.read_parquet(parquet_path)
@@ -48,51 +50,61 @@ st.subheader("Raw data")
 st.write(data.head())
 
 st.subheader("""A look at all the cities in the data""")
-#get the unique cities in the dataframe and cache the data
+# get the unique cities in the dataframe and cache the data
 @st.cache
 def get_cities(data):
     # dropping duplicates
-    cities_unique = data.drop_duplicates(subset = ["City", "Country"])
-    #extract the city and country columns
+    cities_unique = data.drop_duplicates(subset=["City", "Country"])
+    # extract the city and country columns
     cities_countries = cities_unique["City"] + ", " + cities_unique["Country"]
-    cities_countries = cities_countries.reset_index(drop = True).compute()
-    cities_unique = cities_unique.rename(columns={"Latitude":"lat", "Longitude":"lon"})
+    cities_countries = cities_countries.reset_index(drop=True).compute()
+    cities_unique = cities_unique.rename(
+        columns={"Latitude": "lat", "Longitude": "lon"}
+    )
     latlon = cities_unique[["lat", "lon"]].reset_index(drop=True)
     latlon = latlon.compute()
     return latlon, cities_countries
-    
+
+
 latlon, cities_countries = get_cities(data)
 
 # clean the dataframe to get their longitudes and lattitudes
-df = pd.DataFrame(np.empty((len(latlon),2)), columns=['lat', 'lon'])
+df = pd.DataFrame(np.empty((len(latlon), 2)), columns=["lat", "lon"])
 for i in range(len(latlon)):
-    if latlon.iloc[i,0][-1] == 'N':
-        df.iloc[i,0] = float(latlon.iloc[i,0][:-1])
+    if latlon.iloc[i, 0][-1] == "N":
+        df.iloc[i, 0] = float(latlon.iloc[i, 0][:-1])
         pass
     else:
-        df.iloc[i,0] = float(latlon.iloc[i,0][:-1]) * -1
-    if latlon.iloc[i,1][-1] == 'E':
-        df.iloc[i,1] = float(latlon.iloc[i,1][:-1])
+        df.iloc[i, 0] = float(latlon.iloc[i, 0][:-1]) * -1
+    if latlon.iloc[i, 1][-1] == "E":
+        df.iloc[i, 1] = float(latlon.iloc[i, 1][:-1])
         pass
     else:
-        df.iloc[i,1] = float(latlon.iloc[i,1][:-1]) * -1
+        df.iloc[i, 1] = float(latlon.iloc[i, 1][:-1]) * -1
 st.map(df)
-st.caption("""Each dot on the map displays all the cities in the data. The algorithm doesn't support lattitude and longitudes as entry yet.""")
+st.caption(
+    """Each dot on the map displays all the cities in the data. The algorithm doesn't support lattitude and longitudes as entry yet."""
+)
 
 st.subheader("Make your predictions")
-st.write("""Toggle through the following cities and select a year to see the prediction for the average temperature for that year.
-            You can choose to display the temperature in Fahrenheit or Celsius.""")
+st.write(
+    """Toggle through the following cities and select a year to see the prediction for the average temperature for that year.
+            You can choose to display the temperature in Fahrenheit or Celsius."""
+)
 
-#make selection tools to give inputs
+# make selection tools to give inputs
 
 col1, col2 = st.columns(2)
 with col1:
-    city_option = st.selectbox("Which city would you like to choose?", options = cities_countries)
+    city_option = st.selectbox(
+        "Which city would you like to choose?", options=cities_countries
+    )
     fahrenheit = st.checkbox("Temperature in Fahrenheit")
 with col2:
     year = st.slider("What year would you want forecasted?", 2022, 2300, 2050)
 
 city, country = city_option.split(", ")
+
 
 def query_city_data(city, country, ddf):
     """Query the City Data and return mean temperature of the city in each year"""
@@ -103,7 +115,8 @@ def query_city_data(city, country, ddf):
     # Return the dataframe
     return df_city
 
-#make the prediction
+
+# make the prediction
 def predict(city, country, year, fahrenheit):
     """Main function that runs the linear regression"""
     # query the data
@@ -120,18 +133,29 @@ def predict(city, country, year, fahrenheit):
         temp = lr.predict(year_array)
     return temp, df_city
 
+
 temp, df_city = predict(city, country, year, fahrenheit)
-temp_1800 = df_city.iloc[0,1]
+temp_1800 = df_city.iloc[0, 1]
 df_new = df_city[["year", "AverageTemperature"]].set_index("year")
 
 col1, col2 = st.columns(2)
 with col1:
     if fahrenheit:
         temp_1800 = temp_1800 * 9 / 5 + 32
-        st.metric(f"Forecasted Average Temperature", f"{int(temp):.2f}°F", f"{int(temp - temp_1800):.2f}°F")
+        st.metric(
+            f"Forecasted Average Temperature",
+            f"{int(temp):.2f}°F",
+            f"{int(temp - temp_1800):.2f}°F",
+        )
     else:
-        st.metric(f"Forecasted Average Temperature", f"{int(temp):.2f}°C", f"{int(temp - temp_1800):.2f}°C")
-    st.caption("The delta value signifies the change in annual temperature since year 1800.")
+        st.metric(
+            f"Forecasted Average Temperature",
+            f"{int(temp):.2f}°C",
+            f"{int(temp - temp_1800):.2f}°C",
+        )
+    st.caption(
+        "The delta value signifies the change in annual temperature since year 1800."
+    )
 
 with col2:
     if fahrenheit:
